1dc5adb51ec464fea962836402ed7ae326b59da8
joao <jprfsousa@gmail.com>
2023-09-21 09:08:50 -0300

    database created

--- database/schema_descomplica_db.sql
+++ database/schema_descomplica_db.sql
@@ -8,22 +8,22 @@ CREATE TABLE
     IF NOT EXISTS Video (
         EpisodeNumber INTEGER PRIMARY KEY AUTOINCREMENT,
         ArticleDoi TEXT NOT NULL,
-        FOREIGN KEY (ArticleDoi) REFERENCES Article(ArticleDoi),
         Title TEXT NOT NULL,
         URL TEXT NOT NULL,
-        PublishedDate DATE NOT NULL
+        PublishedDate TEXT NOT NULL,
+        FOREIGN KEY (ArticleDoi) REFERENCES Article(ArticleDoi)
     );
 
 CREATE TABLE
     IF NOT EXISTS KeyWord (
-        KeyWordId INTEGER AUTOINCREMENT PRIMARY KEY,
+        KeyWordId INTEGER PRIMARY KEY AUTOINCREMENT,
         Word TEXT NOT NULL UNIQUE
     );
 
 CREATE TABLE
     IF NOT EXISTS videoKeyWord (
-        EpisodeNumber INTEGER NOT NULL,
-        KeyWordId INTEGER NOT NULL,
+        EpisodeNumber INTEGER,
+        KeyWordId INTEGER,
         PRIMARY KEY(EpisodeNumber, KeyWordId),
         FOREIGN KEY (EpisodeNumber) REFERENCES Video(EpisodeNumber),
         FOREIGN KEY (KeyWordId) REFERENCES KeyWord(KeyWordId)

8092ebfd965cb64f2495ab822fe614ca21478b8c
o-rovida <jprfsousa@gmail.com>
2023-10-19 01:24:42 -0300

    copiar html aprimorado

--- library.py
+++ library.py
@@ -8,12 +8,32 @@ def generate_html(tab_id):
     
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    html = soup.prettify()
+
+    html_basic = """
+    <!DOCTYPE html>
+    <html lang="pt-BR">
+    """
+
+    head = soup.find('head')
+    head = str(head)
+
+    search_bar = soup.find('input', {'id': 'search'})
+    search_bar = str(search_bar)
 
     organization_table = soup.find('table', {'id': 'organization_table'})
     organization_table = str(organization_table)
+
+    soup.find('head').extract()
     
-    pc.copy(organization_table)
+    script_list = []
+
+    for script in soup.find_all('script'):
+        script_list.append(str(script))
+
+    html = [html_basic, head, '<body>', '<div>',search_bar, '</div> <div>',organization_table, '</div>'] + script_list + ['</body>','</html>']
+    html = '\n'.join(html)
+    
+    pc.copy(html)
 
     return organization_table
 
